2b48f5f63fa6b57f2ad60bf3bfb7fc8d78a9fc29
Akanksha Rai <akanksharai@Akankshas-MacBook-Pro.local>
2024-09-28 16:21:30 -0400

    deadlines

--- snitch/src/components/DeadlineDisplay.js
+++ snitch/src/components/DeadlineDisplay.js
@@ -2,14 +2,24 @@ import React from 'react';
 import {
     ChakraProvider, 
     Radio,
-    RadioGroup
+    RadioGroup,
+    Stack
   } from '@chakra-ui/react';
   
 function DeadlineDisplay() {
     return(
-        <div>
-            Deadlines!
-        </div>
+       <div>
+        <h1> Deadlines </h1>
+       <RadioGroup defaultValue='1'>
+        <Stack>
+          <Radio value='1'>
+            EECS 281 Project
+          </Radio>
+          <Radio value='2'>EECS HW</Radio>
+          <Radio value='3'>MATH 215 </Radio>
+        </Stack>
+      </RadioGroup>
+      </div>
     );
 }
 export default DeadlineDisplay;
